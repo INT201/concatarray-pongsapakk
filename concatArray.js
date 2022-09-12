599ef5bdf1b080279f2f3b9967e13c6047723c8b
pongsapakk <pongsapak.ratta@mail.kmutt.ac.th>
2022-09-12 15:34:03 +0700

    ahhhhhhhh

--- concatArray.js
+++ concatArray.js
@@ -1,6 +1,17 @@
 const { template } = require('@babel/core')
 
 function concatArray(array1, array2) {
-  //code here
+
+ if((array1 === null || array1 === undefined) && (array2 === undefined || array2 === null)) {
+  return undefined
+ }else if(array1 === [] && array2 === []) {
+  return []
+ }else if(array1 === null || array1 === undefined || array1 === []) {
+ return array2
+ }else if(array2 === null || array2 === undefined  || array2 === []) {
+  return array1
+ }
+  return array1.concat(array2)
 }
 module.exports = concatArray
+
